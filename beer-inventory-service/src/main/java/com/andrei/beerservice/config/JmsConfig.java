68fb155c3c177f888bccefcb2d4c82fdda5d6b73
abalzan <andreibalzan@gmail.com>
2020-02-20 06:59:17 +0000

    configuring JMS for each project

---
+++ beer-inventory-service/src/main/java/com/andrei/beerservice/config/JmsConfig.java
@@ -0,0 +1,20 @@
+package com.andrei.beerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    @Bean //serialize message content to json using Text Message
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+}
