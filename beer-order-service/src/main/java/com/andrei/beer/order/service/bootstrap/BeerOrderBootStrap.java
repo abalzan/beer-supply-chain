e15a3dafc545f0127c2e70c90818b95ed1ca41ac
abalzan <andreibalzan@gmail.com>
2020-06-17 16:11:13 +0100

    fixing tasting room issue!

--- beer-order-service/src/main/java/com/andrei/beer/order/service/bootstrap/BeerOrderBootStrap.java
+++ beer-order-service/src/main/java/com/andrei/beer/order/service/bootstrap/BeerOrderBootStrap.java
@@ -26,8 +26,8 @@ public class BeerOrderBootStrap implements CommandLineRunner {
     }
 
     private void loadCustomerData() {
-        if (customerRepository.count() == 0) {
-            final Customer savedCustomer = customerRepository.save(Customer.builder()
+        if (customerRepository.findAllByCustomerNameLike(TASTING_ROOM).isEmpty()) {
+            final Customer savedCustomer = customerRepository.saveAndFlush(Customer.builder()
                     .customerName(TASTING_ROOM)
                     .apiKey(UUID.randomUUID())
                     .build());
