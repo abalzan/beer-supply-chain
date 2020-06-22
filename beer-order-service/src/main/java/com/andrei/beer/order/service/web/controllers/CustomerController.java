42ee3927b942e55bdd8176b9d50e7531b12eeabe
abalzan <andreibalzan@gmail.com>
2020-06-22 05:59:06 +0100

    adding route for customers and orders Fixing issue customer url

--- beer-order-service/src/main/java/com/andrei/beer/order/service/web/controllers/CustomerController.java
+++ beer-order-service/src/main/java/com/andrei/beer/order/service/web/controllers/CustomerController.java
@@ -19,7 +19,7 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
-    @GetMapping("orders")
+    @GetMapping
     public CustomerPagedList listCustomers(@RequestParam(value = "pageNumber", required = false) Integer pageNumber,
                                            @RequestParam(value = "pageSize", required = false) Integer pageSize) {
 
--- brewery-gateway/src/main/java/com/andrei/brewerygateway/config/LocalHostRouteConfig.java
+++ brewery-gateway/src/main/java/com/andrei/brewerygateway/config/LocalHostRouteConfig.java
@@ -15,6 +15,10 @@ public class LocalHostRouteConfig {
                 .route(predicateSpec -> predicateSpec.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                         .uri("http://localhost:8080")
                         .id("beer-service"))
+                .route(predicateSpec -> predicateSpec.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081")
+                        .id("order-service"))
+
                 .build();
     }
 }
