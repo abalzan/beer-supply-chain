61918730b7f8d1bedb16a7599e0fbb7c6ba1b5a0
abalzan <andreibalzan@gmail.com>
2020-07-09 17:05:55 +0100

    Circuit breaker inventory failover first commit

---
+++ brewery-inventory-failover/src/main/java/com/andrei/brewery/inventoryfailover/BreweryInventoryFailoverApplication.java
@@ -0,0 +1,13 @@
+package com.andrei.brewery.inventoryfailover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryInventoryFailoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryInventoryFailoverApplication.class, args);
+	}
+
+}
---
+++ brewery-inventory-failover/src/test/java/com/andrei/brewery/inventoryfailover/BreweryInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrei.brewery.inventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryInventoryFailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
