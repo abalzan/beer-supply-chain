a9fdb93db6e5038698c9137b0340ad93e9d73add
abalzan <andreibalzan@gmail.com>
2020-07-15 15:39:39 +0100

    initial commit config server

---
+++ brewery-config-server/src/main/java/com/andrei/brewery/configserver/BreweryConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.andrei.brewery.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryConfigServerApplication.class, args);
+    }
+
+}
