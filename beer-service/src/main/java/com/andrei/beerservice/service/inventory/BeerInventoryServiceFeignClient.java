da1d91799fc6ca625b0360556b860e07162ac30f
abalzan <andreibalzan@gmail.com>
2020-08-03 10:43:23 +0100

    Http basic authentication for feign client

--- beer-service/src/main/java/com/andrei/beerservice/service/inventory/BeerInventoryServiceFeignClient.java
+++ beer-service/src/main/java/com/andrei/beerservice/service/inventory/BeerInventoryServiceFeignClient.java
@@ -1,5 +1,6 @@
 package com.andrei.beerservice.service.inventory;
 
+import com.andrei.beerservice.config.FeignClientConfig;
 import com.andrei.beerservice.service.inventory.model.BeerInventoryDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "beer-inventory-service", fallback = BeerInventoryServiceFeignClientFailover.class)
+@FeignClient(name = "beer-inventory-service", fallback = BeerInventoryServiceFeignClientFailover.class, configuration = FeignClientConfig.class)
 public interface BeerInventoryServiceFeignClient {
 
     @GetMapping(value = BeerInventoryServiceRestTemplate.INVENTORY_PATH)
